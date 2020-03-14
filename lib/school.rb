e3a69ebdd08f42a06aeb31e8e2add241f68702b1
Ruth Kebede <rutht.kebede@gmail.com>
2020-03-14 01:13:08 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,8 +1,14 @@
 class School 
-  attr_accessor = :roster
 
   def initialize(school)
     @school = school
     @roster = {}
   end 
+  
+  def roster=(roster)
+    @roster = roster
+  end 
+  
+  def roster 
+    @roster = roster
 end 
